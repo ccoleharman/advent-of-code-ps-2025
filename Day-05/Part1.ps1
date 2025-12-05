60b6119ad5d51427e3d775e3320e5ccf2d016b58
ccoleharman <harmanationbuilds@gmail.com>
2025-12-05 10:53:36 -0600

    Day 05 Complete

---
+++ Day-05/Part1.ps1
@@ -0,0 +1,23 @@
+﻿$input = Get-Content input.txt
+
+$ranges = @()
+$i = 0
+while ($input[$i] -ne ""){
+    $ranges += ,@([int64]$input[$i].split("-")[0], [int64]$input[$i].split("-")[1])
+    $i += 1
+}
+$ranges = $ranges | Sort-Object { $_[0] }
+
+$count = 0
+$i += 1
+while ($i -lt $input.Length){
+    for ($j = 0; $j -lt $ranges.Length -and [int64]$input[$i] -ge $ranges[$j][0]; $j += 1){
+        if ([int64]$input[$i] -le $ranges[$j][1]) {
+            $count += 1
+            break  
+        }
+    }
+    $i += 1
+}
+
+write $count
---
+++ Day-05/Part2.ps1
@@ -0,0 +1,25 @@
+﻿$input = Get-Content input.txt
+
+$ranges = @()
+$i = 0
+while ($input[$i] -ne ""){
+    $ranges += ,@([int64]$input[$i].split("-")[0], [int64]$input[$i].split("-")[1])
+    $i += 1
+}
+$ranges = $ranges | Sort-Object { $_[0] }
+
+$count = 0
+$last = 0
+foreach ($range in $ranges){
+    if ($range[0] -le $last -and $range[1] -gt $last){
+        $count += $range[1] - $last
+        $last = $range[1]
+    }
+    elseif ($range[0] -gt $last){
+        $count += $range[1] - $range[0]
+        $count += 1
+        $last = $range[1]
+    }
+}
+
+write $count
