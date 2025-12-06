4089599ff27bb17a81f8e1c22988ec261222f72e
ccoleharman <harmanationbuilds@gmail.com>
2025-12-06 09:03:08 -0600

    Day 06 Complete

---
+++ Day-06/Part1.ps1
@@ -0,0 +1,19 @@
+﻿$input = Get-Content input.txt | ForEach-Object { ,(($_ -replace '\s+', ' ').Trim().Split(" ")) }
+
+$sum = 0
+for ($j = 0; $j -lt $input[0].Length; $j += 1){
+    $nums = @()
+    for ($i = 0; $i -lt $input.Length - 1; $i += 1){
+        $nums += ,[int]$input[$i][$j]
+    }
+    if ($input[$input.Length - 1][$j] -eq "*"){
+        $product = 1
+        $nums | ForEach-Object { $product *= $_ }
+        $sum += $product
+    }
+    else {
+        $nums | ForEach-Object { $sum += $_ }
+    }
+}
+
+write $sum
---
+++ Day-06/Part2.ps1
@@ -0,0 +1,51 @@
+﻿$input = Get-Content input.txt
+
+$sum = 0
+$nums = @()
+$mult = $true
+$AllSpace = $false
+for ($j = 0; $j -lt $input[0].Length; $j += 1){
+    if ($AllSpace) {
+        if ($mult){
+            $product = 1
+            $nums | ForEach-Object { $product *= $_ }
+            $sum += $product
+        }
+        else {
+            $nums | ForEach-Object { $sum += $_ }
+        }
+        $AllSpace = $false
+        if ($input[$input.Length - 1][$j] -eq "*"){
+            $mult = $true
+        }
+        else {
+            $mult = $false
+        }
+        $nums = @()
+    }
+
+    $AllSpace = $true
+    $num = ""
+    for ($i = 0; $i -lt $input.Length - 1; $i += 1){
+        if ($input[$i][$j] -eq " "){
+            continue
+        }
+        else {
+            $AllSpace = $false
+            $num += $input[$i][$j]
+        }
+    }
+    if ($num -ne ""){
+        $nums += ,[int]$num
+    }
+}
+if ($mult){
+    $product = 1
+    $nums | ForEach-Object { $product *= $_ }
+    $sum += $product
+}
+else {
+    $nums | ForEach-Object { $sum += $_ }
+}
+
+write $sum
